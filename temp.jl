15d74fc046d917f49308b5473b65a85e8c902629
thesharpshooter <prakash9266@gmail.com>
2017-04-03 01:56:39 +0530

    Debugged test case

--- temp.jl
+++ temp.jl
@@ -5,20 +5,17 @@ A[end,1] = 1
 A[1,end] = 1
 g(t,u) = 2-u
 u0 = zeros(11); u0[6] = 1
-nsteps = 30;
+nsteps = 1000;
 tmax = 10.0;
 h = tmax/nsteps;
 u = u0;
 t = 0
-
-phi = expm(-h*A)
-#################SOLUTION########################
+I = eye(11)
+u = zeros(11);u[6]=1
+phi1 = ((expm(h*A)-I)/A)
 for k in 1:nsteps
-	phi = (phi-1)\(-A)
-	u = expm(-h*A)*u + phi*g(t,u)
-	t = k*h
-	println("U is : ",u)
-	println("step: ",k)
+  u += phi1*(A*u + g(t,u))
+  t = k*h
 end
-println("The final value of U is : ",u)
+@show u
 
